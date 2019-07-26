40416b64d3d6fbf7b2a31893ea9a961c9f63a811
stant <stant@stants-Air.home>
2019-07-26 16:21:08 +0200

    ios project

--- platforms/ios/platform_www/cordova_plugins.js
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -10,8 +10,8 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
     }
   ];
   module.exports.metadata = {
-    "cordova-plugin-whitelist": "1.3.3",
     "cordova-plugin-add-swift-support": "2.0.1",
+    "cordova-plugin-whitelist": "1.3.3",
     "cordova-plugin-zeroconf": "1.4.0"
   };
 });
